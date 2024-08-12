d78df36356abc50114db343bd7ccdacdc61ec625
guilhermeHB28 <guilherme.batista28@escola.pr.gov.br>
2024-08-12 12:16:33 -0300

    Add files via upload

---
+++ js/banner.js
@@ -0,0 +1,34 @@
+const bannerImages = [
+    "assets/img/blocao de terra 1mx1m.png  ",
+    "assets/img/stru.png",
+    "assets/img/ouro.png"
+    
+  ];
+  
+  // Index to track the current image
+  let currentIndex = 0;
+  
+  // Function to change the banner image
+  function changeBannerImage() {
+    const bannerImageElement = document.getElementById('bannerImage');
+    currentIndex = (currentIndex + 1) % bannerImages.length;
+    bannerImageElement.src = bannerImages[currentIndex];
+  }
+  
+  // Change banner image every 3 seconds
+  setInterval(changeBannerImage, 3000);
+  
+  // Function to open the modal
+  function openModal(src) {
+    const modal = document.getElementById('myModal');
+    const modalImage = document.getElementById('modalImage');
+    modal.style.display = "block";
+    modalImage.src = src;
+  }
+  
+  // Function to close the modal
+  function closeModal() {
+    const modal = document.getElementById('myModal');
+    modal.style.display = "none";
+  }
+  
